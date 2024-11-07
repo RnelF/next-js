6f0fed673dbde8b3f190015bbef2c79dacf0e5cb
Arnel Francia <nelffff@gmail.com>
2024-11-07 20:38:54 +0800

    server route handling and revalidatPath function

--- app/mock-users/page.tsx
+++ app/mock-users/page.tsx
@@ -1,3 +1,4 @@
+import { revalidatePath } from "next/cache"; //usefull for form submissions doesn't need to refresh the page to render new submissions
 type MockUser = {
   id: number;
   name: string;
@@ -8,19 +9,53 @@ export default async function MockUsers() {
 
   const users = await res.json();
 
+  async function addUser(formData: FormData) {
+    "use server";
+    const name = formData.get("name");
+    const res = await fetch(
+      "https://672cac311600dda5a9f96ab1.mockapi.io/users",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name }),
+      }
+    );
+    const newUser = await res.json();
+    revalidatePath("/mock-users");
+    console.log(newUser);
+  }
+
   return (
-    <div
-      className="grid grid-cols-4 py-10
-    "
-    >
-      {users.map((user: MockUser) => (
-        <div
-          key={user.id}
-          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+    <div className="py-10">
+      <form action={addUser} className="mb-4">
+        <input
+          type="text"
+          name="name"
+          required
+          className="border shadow-md p-2 mr-2 rounded "
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded"
         >
-          {user.name}
-        </div>
-      ))}
+          Add User
+        </button>
+      </form>
+      <div
+        className="grid grid-cols-4 py-10
+    "
+      >
+        {users.map((user: MockUser) => (
+          <div
+            key={user.id}
+            className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+          >
+            {user.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
